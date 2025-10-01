245d56501cd15eb4ff7561e36ce985b6a4ee4d77
devsudu <sudarshanashoksamaje@gmail.com>
2025-10-02 00:03:12 +0530

    Api gateway implementation

---
+++ src/main/java/dev/sudu/apigatewaysept28/ApiGatewaySept28Application.java
@@ -0,0 +1,13 @@
+package dev.sudu.apigatewaysept28;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewaySept28Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewaySept28Application.class, args);
+    }
+
+}
---
+++ src/test/java/dev/sudu/apigatewaysept28/ApiGatewaySept28ApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sudu.apigatewaysept28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewaySept28ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
